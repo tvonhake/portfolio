8a08ed883489ae06aedf12128ac685e51c2a0a17
Trevor von Hake <tvonhake@outlook.com>
2021-05-01 23:53:13 -0600

    projects completed

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -6,70 +6,165 @@
     grid-list-lg
     style="max-height: 100vh; max-width: 100vw"
   >
-    <v-layout column pa-5 style="margin-top: 20px">
-      <v-layout row wrap mt-5>
-        <v-flex xs12 sm12 md4>
-          <v-card>
-            <v-img src="https://i.imgur.com/6FL7jsq.png" height="500px">
-              <v-container fill-height fluid>
-                <v-layout fill-height>
-                  <v-flex xs12 align-end flexbox> </v-flex>
-                </v-layout>
-              </v-container>
-            </v-img>
+    <v-slide-group
+      v-model="model"
+      class="pa-0 ma-0"
+      show-arrows
+      center-active
+      dark
+    >
+      <v-slide-item v-for="n in 1" :key="n" v-slot="{ toggle }">
+        <v-hover v-slot:default="{ hover }">
+          <v-flex xs12 sm12 md4 mx-10>
+            <v-card
+              :color="transparent"
+              class="ma-4"
+              height="75vh"
+              width="375px"
+              @click="toggle"
+            >
+              <v-img src="https://i.imgur.com/6FL7jsq.png" height="500px">
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-fast-in-fast-out grey darken-2 v-card--reveal display-3 white--text"
+                    style="height: 100%"
+                  >
+                    A digital replacement for physical vaccination proof
+                  </div>
+                </v-expand-transition>
+              </v-img>
 
-            <v-card-title primary-title>
-              <div>
-                <h3 class="headline mb-0">Vaccine Passport</h3>
-                <div>Description here.</div>
-              </div>
-            </v-card-title>
-          </v-card>
-        </v-flex>
+              <v-card-title primary-title>
+                <v-row dense>
+                  <h3 class="headline mb-0 mr-5">Vaccine Passport</h3>
+                  <v-spacer />
+                  <v-btn
+                    medium
+                    color="transparent"
+                    depressed
+                    dark
+                    mr-n8
+                    href="https://vaccine-passport.herokuapp.com/"
+                    target="_blank"
+                  >
+                    <v-icon medium color="white">{{ Web }}</v-icon>
+                  </v-btn>
+                  <v-btn
+                    medium
+                    color="transparent"
+                    depressed
+                    dark
+                    href="https://github.com/Derek229/vaccine-passport"
+                    target="_blank"
+                  >
+                    <v-icon medium color="white">{{ GitHub }}</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-hover>
+      </v-slide-item>
 
-        <v-flex xs12 sm12 md4>
-          <v-card>
-            <v-img src="https://i.imgur.com/3ThKo6y.png" height="500px">
-              <v-container fill-height fluid>
-                <v-layout fill-height>
-                  <v-flex xs12 align-end flexbox> </v-flex>
-                </v-layout>
-              </v-container>
-            </v-img>
-            <v-card-title primary-title>
-              <div>
-                <h3 class="headline mb-0">Musn</h3>
-                <div>Description here</div>
-              </div>
-            </v-card-title>
-          </v-card>
-        </v-flex>
+      <v-slide-item v-for="n in 1" :key="n" v-slot="{ toggle }">
+        <v-hover v-slot:default="{ hover }">
+          <v-flex xs12 sm12 md4 mx-10>
+            <v-card
+              :color="transparent"
+              class="ma-4"
+              height="75vh"
+              width="375px"
+              @click="toggle"
+            >
+              <v-img src="https://i.imgur.com/7nMgRVt.png" height="500px">
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-fast-in-fast-out grey darken-2 v-card--reveal display-3 white--text"
+                    style="height: 100%"
+                  >
+                    React Native Mobile App for Vaccine Passport
+                  </div>
+                </v-expand-transition>
+              </v-img>
+              <v-card-title primary-title>
+                <v-row dense>
+                  <h3 class="headline mb-0">Vaccine Passport Mobile</h3>
+                  <v-spacer />
+                  <v-btn
+                    medium
+                    color="transparent"
+                    depressed
+                    dark
+                    href="https://github.com/tvonhake/VaccinePassport-ReactNativeWebView"
+                    target="_blank"
+                  >
+                    <v-icon medium color="white">{{ GitHub }}</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-hover>
+      </v-slide-item>
 
-        <v-flex xs12 sm12 md4>
-          <v-card>
-            <v-img src="https://i.imgur.com/7nMgRVt.png" height="500px">
-              <v-container fill-height fluid>
-                <v-layout fill-height>
-                  <v-flex xs12 align-end flexbox> </v-flex>
-                </v-layout>
-              </v-container>
-            </v-img>
-            <v-card-title primary-title>
-              <div>
-                <h3 class="headline mb-0">Vaccine Passport App</h3>
-                <div>Description here</div>
-              </div>
-            </v-card-title>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-layout>
+      <v-slide-item v-for="n in 1" :key="n" v-slot="{ toggle }">
+        <v-hover v-slot:default="{ hover }">
+          <v-flex xs12 sm12 md4 mx-10>
+            <v-card
+              :color="transparent"
+              class="ma-4"
+              height="75vh"
+              width="375px"
+              @click="toggle"
+            >
+              <v-img src="https://i.imgur.com/3ThKo6y.png" height="500px">
+                <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-fast-in-fast-out grey darken-2 v-card--reveal display-3 white--text"
+                    style="height: 100%"
+                  >
+                    community-driven social media for music sharing
+                  </div>
+                </v-expand-transition>
+              </v-img>
+              <v-card-title primary-title>
+                <v-row dense>
+                  <h3 class="headline mb-0 mr-16">Musn</h3>
+                  <v-spacer />
+                  <v-btn
+                    medium
+                    color="transparent"
+                    depressed
+                    dark
+                    ml-16
+                    mr-0
+                    href="https://github.com/Derek229/Musn"
+                    target="_blank"
+                  >
+                    <v-icon medium color="white">{{ GitHub }}</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-hover>
+      </v-slide-item>
+    </v-slide-group>
   </v-container>
 </template>
 
 <script>
+import { mdiGithub, mdiWeb } from '@mdi/js';
 export default {
   name: 'Projects',
+  data: () => ({
+    model: null,
+    Web: mdiWeb,
+    GitHub: mdiGithub,
+  }),
 };
 </script>
 
@@ -79,5 +174,13 @@ export default {
   background-size: cover;
   width: 100% !important;
   height: 100%;
+}
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.9;
+  position: absolute;
+  width: 100%;
 }
 </style>
